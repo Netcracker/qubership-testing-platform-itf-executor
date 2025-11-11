a2392122a91df676cf1309e2a9ebaddb7b2bbeb4
dmch1019 <dmitrii.churbanov@netcracker.com>
2025-11-11 13:26:59 +0500

    feat: add method removeHazelcastContextSizeCountToProject;

--- mockingbird-core/src/main/java/org/qubership/automation/itf/core/metric/MetricsAggregateService.java
+++ mockingbird-core/src/main/java/org/qubership/automation/itf/core/metric/MetricsAggregateService.java
@@ -142,8 +142,8 @@ public class MetricsAggregateService {
         meterRegistry.getMeters().stream()
                 .filter(meter -> meter.getId().getName().equals(
                         Metric.ATP_ITF_EXECUTOR_HAZELCAST_CONTEXT_SIZE_BY_PROJECT.getValue()))
-                .filter(m -> projectUuid.toString().equals(m.getId().getTag(MetricTag.PROJECT.getValue()))
-                        && contextId.equals(m.getId().getTag(MetricTag.CONTEXT_ID.getValue())))
+                .filter(m -> projectUuid.toString().equals(m.getId().getTag(MetricTag.PROJECT.getValue())))
+                .filter(m -> contextId.toString().equals(m.getId().getTag(MetricTag.CONTEXT_ID.getValue())))
                 .forEach(meterRegistry::remove);
     }
 
